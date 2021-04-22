d9f9f941932bda174560542c0e7c8404ad45e260
Octavio Villena <octaviovillena@hotmail.com>
2021-04-22 13:15:18 -0700

    connection

--- databaseConnection.js
+++ databaseConnection.js
@@ -6,9 +6,9 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
   host: "us-cdbr-east-03.cleardb.com",
-  user: "bec80d8e3186f3",
-  password: "66bb8dd2",
-  database: "heroku_57f1eadd63b46f9",
+  user: "b762967cfa65b8",
+  password: "2391fbbd",
+  database: "heroku_545694f0e4d4311",
   multipleStatements: false,
   namedPlaceholders: true,
 };
